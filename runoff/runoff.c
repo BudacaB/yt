3e9472830cf813977e6752b02f3a301123843386
BudacaB <budaca.b@gmail.com>
2020-06-29 03:41:43 +0300

    correction

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,7 +147,7 @@ void tabulate(void)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        for (int j = 0; i < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (!candidates[preferences[i][j]].eliminated)
             {
